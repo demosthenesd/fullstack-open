d726699e3d4c45d6e3c479a8e86e4a7506f103a5
demosthenesd <demosthenesd@icloud.com>
2025-03-29 10:09:48 +1000

    Finished reading part1 - Intro to react

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,22 +1,29 @@
-const Hello = () => {
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world</p>
+      <p>
+        Hello world ka {props.name} nga tigols with the age of {props.age}
+      </p>
     </div>
-  )
-}
+  );
+};
+
+const Footer = () => {
+  return <footer>Thiss a footer babyy</footer>;
+};
 
 const App = () => {
+  const name = "Peter";
+  const age = 10;
+
   return (
-    <div>
+    <>
       <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+      <Footer />
+    </>
+  );
+};
 
-      <Hello />
-      <Hello />
-      <Hello />
-      <Hello />
-      </div>
-  )
-}
-
-export default App
+export default App;
