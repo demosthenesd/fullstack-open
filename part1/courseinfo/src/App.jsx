01065a3de63f203c11ff4230034c81f8f16c96ad
demosthenesd <demosthenesd@icloud.com>
2025-03-29 16:43:03 +1000

    part1 progress pt 2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,54 +1,65 @@
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   const Header = (props) => {
-return(
-    <div>
-      <p>{props.course}</p>
-    </div>
-)
-  }
-
+    console.log(props);
+    return (
+      <div>
+        <p>{props.course}</p>
+      </div>
+    );
+  };  
 
   const Content = (props) => {
-return (
-    <div>
-      <p>{props.part}: {props.exercises}</p>
-    </div>
-)
-  }
+    return (
+      <div>
+        <p>
+          {props.name}: {props.exercises}
+        </p>
+      </div>
+    );
+  };
+
+
+  const Part = (props) => {
+    return (
+      <div>
+        <p>
+          {props.part.name}: {props.part.exercises}
+        </p>
+      </div>
+    );
+  };
 
   const Total = (props) => {
-return (
-
-  <div>
-      <p>Total is: {props.total}</p>
-    </div>
-
-)
-}
-
-
-
-
-
+    return (
+      <div>
+        <p>Total is: {props.total}</p>
+      </div>
+    );
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
-      <Total total={exercises1 + exercises2 + exercises3} />
-
+      <Part part={part1} />
+      <Part part={part2} />
+      <Part part={part3} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   );
 };
 
-export default App
+export default App;
