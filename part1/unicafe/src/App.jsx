926c511e3508d1bf1a771b9590abd48868c7daa1
demosthenesd <demosthenesd@icloud.com>
2025-04-01 11:00:11 +1000

    Finished unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,23 +1,47 @@
 import { useState } from 'react'
 
-
-
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 
-const Statistics = ({ good, bad, neutral }) => {
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>
+      {text}
+    </td>
+    <td>
+      {value}
+    </td>
+  </tr>
+)
 
+const Statistics = ({ good, bad, neutral }) => {
+  if (good + bad + neutral === 0) return (
+    <div>
+      <h2>Statistics</h2>
+      <p>
+
+        No feedback given
+      </p>
+    </div>)
+
+  const all = (good + bad + neutral);
+  const avg = ((good * 1) + (bad * -1) + (neutral * 0)) / all || 0;
+  const positive = ((good * 1) / all) * 100 || 0;
 
   return (
     <div>
       <h2>Statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + bad + neutral}</p>
-      <p>avg {((good*1) + (bad* -1) + (neutral*0)) /(good + bad + neutral)}</p>
-    <p>positive {(((good*1) /(good + bad + neutral))*100)}%</p>
-    
+      <table>
+
+
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="avg" value={avg} />
+        <StatisticLine text="positive" value={positive + "%"} />
+
+      </table>
     </div>
   )
 }
@@ -39,9 +63,9 @@ const App = () => {
     <div>
       <h1>give feedback
       </h1>
-      <Button onClick={handleBadClick} text={"bad"} />
-      <Button onClick={() => setNeutral(neutral + 1)} text={"neutral"} />
       <Button onClick={handleGoodClick} text={"good"} />
+      <Button onClick={() => setNeutral(neutral + 1)} text={"neutral"} />
+      <Button onClick={handleBadClick} text={"bad"} />
 
       <Statistics good={good} bad={bad} neutral={neutral} />
 
