6a380915cf3950d1418f3d7c3012e706f6d2ca3f
demosthenesd <demosthenesd@icloud.com>
2025-04-08 16:28:34 +1000

    finished part2c theory + exercise 2.11

--- part2/scratch/src/App.jsx
+++ part2/scratch/src/App.jsx
@@ -1,55 +1,25 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
-import { useState } from 'react'
-
-const App = (props) => {
-
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState(
-    'a new note...'
-  )
-
-  const [showAll, setshowAll] = useState(true);
-
-  const notesToShow = showAll ? notes : notes.filter(note => note.important);
 
 
-  const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      important: Math.random() < 0.5,
-      id: String(notes.length + 1),
-    }
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
   }
-
-  const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
-  }
-
-  return (
-    <div>
-      <h1>Notes</h1>
-      <>
-        <button onClick={() => setshowAll(!showAll)}>
-          show {showAll ? 'important' : 'all'}
-        </button>
-      </>
-      <ul>
-
-        {notesToShow.map(note =>
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
-
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type="submit">save</button>
-      </form>
-    </div>
-  )
+  
+  useEffect(hook, [])
 }
-export default App
+
+export default App;
