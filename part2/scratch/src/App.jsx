8ac3d91562e9e978dfa5dada4b68328b7eb599bf
demosthenesd <demosthenesd@icloud.com>
2025-04-09 22:16:47 +1000

    tiny progress

--- part2/scratch/src/App.jsx
+++ part2/scratch/src/App.jsx
@@ -41,7 +41,7 @@ const App = () => {
     const url = `http://localhost:3001/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
-  
+
     axios.put(url, changedNote).then(response => {
       setNotes(notes.map(n => n.id === id ? response.data : n))
     })
