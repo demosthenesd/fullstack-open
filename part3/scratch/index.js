a4f4b891795185b9dd7e5f496ed9c48c80c882ab
demosthenesd <demosthenesd@icloud.com>
2025-04-12 17:49:37 +1000

    progress on part3A theory

--- part3/scratch/index.js
+++ part3/scratch/index.js
@@ -1,11 +1,42 @@
-const http = require('http');
-
-const app = http.createServer((req,res)=> {
-    res.writeHead(200, {"Content-type" :'text/plain'});
-    res.end("Hellow world");
-})
+const express = require('express')
+const app = express()
 
 
-const PORT =3001;
-app.listen(PORT);
-console.log(`service running in Port:${PORT}`)
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello wsadWorld wassapxzxcz!</h1>')
+  })
+  
+  app.get('/api/notes', (request, response) => {
+    response.json(notes)
+  })
+  
+  app.get('/api/notes/:id', (request, response) => {
+    const id = request.params.id
+    const note = notes.find(note => note.id === id)
+    response.json(note)
+  })
+
+
+
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
