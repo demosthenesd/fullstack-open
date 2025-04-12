7575159c143c6456cdefc49bfc9e76c6d686ee07
demosthenesd <demosthenesd@icloud.com>
2025-04-12 14:44:58 +1000

    part 3 thoery progress

---
+++ part3/scratch/index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const app = http.createServer((req,res)=> {
+    res.writeHead(200, {"Content-type" :'text/plain'});
+    res.end("Hellow world");
+})
+
+
+const PORT =3001;
+app.listen(PORT);
+console.log(`service running in Port:${PORT}`)
