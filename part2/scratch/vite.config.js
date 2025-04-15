0186716e4fdaa56fc1badb875b88360534133fc8
demosthenesd <demosthenesd@icloud.com>
2025-04-15 22:54:09 +1000

    updated vite file again

--- part2/scratch/vite.config.js
+++ part2/scratch/vite.config.js
@@ -6,6 +6,11 @@ export default defineConfig({
   plugins: [react()],
 
   server: {
+
+    host: true,
+    strictPort: true,
+    port: 8000,
+
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
