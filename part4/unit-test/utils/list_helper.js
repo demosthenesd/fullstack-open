6e1c8da8c91e2840c4bbf16d039f914759e8067b
demosthenesd <demosthenesd@icloud.com>
2025-04-24 23:43:23 +1000

    attempt on extra excercises for part4a

--- part4/unit-test/utils/list_helper.js
+++ part4/unit-test/utils/list_helper.js
@@ -5,13 +5,37 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => {
   if (blogs.length === 1) return blogs[0].likes
-
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+
+const favoriteBlog = (blogs)=> {
+
+  const maxLikes = 0;
+
+
+
+  blogs.forEach(blog =>{
+    if(blog.like > maxLikes) maxLikes = blog.like;
+  })
+
+  return {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0
+  } ;
 
 
 }
 
 
+
+
+
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes,favoriteBlog
 }
