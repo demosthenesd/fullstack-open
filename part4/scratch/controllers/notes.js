73655d2f3f660ac816b2957c29f9c650cc4466fa
demosthenesd <demosthenesd@icloud.com>
2025-04-25 23:46:04 +1000

    more tests in the backend progress

--- part4/scratch/controllers/notes.js
+++ part4/scratch/controllers/notes.js
@@ -1,26 +1,11 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
-notesRouter.get('/', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
-})
-
-notesRouter.get('/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
-      if (note) {
-        response.json(note)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
-})
-
-notesRouter.post('/', (request, response, next) => {
-  const body = request.body
+// notesRouter.get('/', (request, response) => {
+//   Note.find({}).then(notes => {
+//     response.json(notes)
+//   })
+// })
 
   const note = new Note({
     content: body.content,
