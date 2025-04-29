0f5246cbc01fc28192160dedd240f5083be8a630
demosthenesd <demosthenesd@icloud.com>
2025-04-29 15:50:56 +1000

    finished part4d theories

--- part4/scratch/controllers/notes.js
+++ part4/scratch/controllers/notes.js
@@ -2,6 +2,16 @@ const notesRouter = require("express").Router();
 const Note = require("../models/note");
 const User = require("../models/users");
 
+const jwt = require("jsonwebtoken");
+
+const getTokenFrom = (request) => {
+    const authorization = request.get("authorization");
+    if (authorization && authorization.startsWith("Bearer ")) {
+        return authorization.replace("Bearer ", "");
+    }
+    return null;
+};
+
 notesRouter.get("/", async (request, response) => {
     const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
     response.json(notes);
@@ -18,6 +28,12 @@ notesRouter.get("/:id", async (request, response, next) => {
 
 notesRouter.post("/", async (request, response, next) => {
     const body = request.body;
+
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: "token invalid" });
+    }
+    const user = await User.findById(decodedToken.id);
 
     const user = await User.findById(body.userId);
 
